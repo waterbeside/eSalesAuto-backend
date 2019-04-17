e6f726168f264b0fd5171a397978cb913be76e26
waterbeside <waterbeside@163.com>
2019-04-17 12:05:08 +0800

    other: 删除多余

--- app/controller/Base.js
+++ app/controller/Base.js
@@ -17,36 +17,40 @@ class BaseController extends Controller {
     // process.exit(1);
   }
 
-  async getUserData(){
+  /**
+   * 取得用户信息
+   */
+  async getUserData(type = 0){
     const { ctx , app} = this;
-    if(typeof(app.myData)!='undefined' && typeof(app.myData.userBaseData)!='undefined' ){
-      return app.myData.userBaseData;
-    }else{
+    if(typeof(app.myData)=='undefined'){
       return false;
     }
+    if(typeof(app.myData.jwt_decoded)=='undefined'){
+      return false;
+    }
+    app.myData.userBaseData = {
+      "uid" : app.myData.jwt_decoded.uid,
+      "username" : app.myData.jwt_decoded.username,
+    }
+    if(type){
+      let uid = app.myData.jwt_decoded.uid;
+      let res = await ctx.model.User.findByUid(uid);
+      if(res){
+        return {
+          "id":res.id,
+          "username":res.username,
+          "rid":res.rid,
+        }
+      }
+    }else{
+      if(typeof(app.myData.userBaseData)!='undefined' ){
+        return app.myData.userBaseData;
+      }
+    }
+    return false;
+    
   }
-  // async checkLogin() {
-  //   const { ctx,app } = this;
-  //   const jwt = app.jwt;
-  //   let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4MDAwMDIzMzIzMiwidWlkIjoyMiwibmFtZSI6InRlc3QiLCJpYXQiOjE1NTUxMjE2Mzd9.kQyXv1YQVlw1NQLNiEUh_NgSyJ8A-U9eJZYHdztbZPs';
-  //   let decoded = jwt.verify(token, app.config.jwt.secret);
-  //   app.userBaseData = decoded;
-  //   await this.jsonReturn(-1,'你还未登入');
-  //   return false;
-  //   if (ctx.request.header['authorization']) {
-  //     let token = ctx.request.header['authorization'].split(' ')[1];
-  //     console.log(token)
-  //     let decoded;
-  //     //解码token
-  //     try {
-  //       decoded = jwt.verify(token, app.config.jwt.secret);
-  //     } catch (error) {
-  //       if (error.name == 'TokenExpiredError') {
-  //
-  //       }
-  //     }
-  //   }
-  // }
+
 }
 
 module.exports = BaseController;
