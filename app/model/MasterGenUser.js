7f7c5a2ad111d491bf0415e29c5e879917f7c594
waterbeside <waterbeside@163.com>
2019-05-02 13:59:05 +0800

    feat: MasterGenUser Model

---
+++ app/model/MasterGenUser.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = app => {
+  const { STRING, INTEGER, DATE } = app.Sequelize;
+
+  const MasterGenUser = app.model.define('Master_Gen_User', {
+    ID: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    User_ID: STRING(10),
+    Sales_Team: STRING(5),
+    Customer_Code:INTEGER,
+  },
+  {freezeTableName: true,
+    timestamps: false, 
+  }
+);
+
+
+ //通过id查数据
+  MasterGenUser.findByUserID = async function(username) {
+    return await this.findAll({
+      where: { User_ID:username },
+      order : [
+        ['Customer_Code', 'ASC'],
+      ]
+    });
+  };
+
+  //通过id查数据
+  MasterGenUser.findCustomCodeByUserID = async function(username) {
+    let res =  await this.findByUserID(username);
+    let returnData = [];
+    res.forEach(element => {
+      returnData.push(element.Customer_Code);
+    });
+    return returnData;
+  };
+
+  
+
+  return MasterGenUser;
+};
