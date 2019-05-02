978937f6c6e8579ec7ccac8b73e4b07af4f16f05
waterbeside <waterbeside@163.com>
2019-05-02 13:59:38 +0800

    opt: Add base service

---
+++ app/service/Base.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Service = require('egg').Service;
+class BaseService extends Service {
+
+  formatOracleRes(res){
+    let metaData = res.metaData;
+    let rows = res.rows;
+    let list = [];
+
+    rows.forEach((item,index) => {
+      let item_kv = {};
+      metaData.forEach((field,i) => {
+        // console.log(field.name)
+        
+        item_kv[field.name] = item[i];
+      });
+      list[index] = item_kv;
+    });
+
+    return list;
+  }
+  
+}
+
+module.exports = BaseService;
