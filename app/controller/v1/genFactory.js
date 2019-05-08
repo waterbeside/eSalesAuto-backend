13fa0da392f1c0c50c2caff330671bbc85050cb1
waterbeside <waterbeside@163.com>
2019-05-08 12:12:16 +0800

    update

--- app/controller/v1/genFactory.js
+++ app/controller/v1/genFactory.js
@@ -5,17 +5,7 @@ const BaseController = require('../Base');
 class GenFactoryController extends BaseController {
   async getFactoryIds() {
     const { ctx ,app} = this;
-    
-    if(typeof(app.redis)=='object'){
-      const cacheKey = 'SalesAuto:genFactory:factoryIds';
-      let caceData = app.redis.get(cacheKey);
-      if(caceData){
-        console.log(caceData);
-      }
-    }
-    
     const res = await ctx.service.genFactory.getFactoryIDList();
-    console.log(res);
     if(res.length == 0){
       return this.jsonReturn(20002,{list:[]},'No data');
     }
--- app/service/GenFactory.js
+++ app/service/GenFactory.js
@@ -14,12 +14,19 @@ class GenFactoryService extends BaseService {
     // console.log(returnData)
     // return returnData;
 
+    const { ctx , app} = this;
+    let cacheKey = "escm:gen_factorory:factory_id_list";
+    let cacheData = await ctx.helper.getStoreData(cacheKey);
+    if(cacheData){
+      return cacheData;
+    }
     let sql = "SELECT DISTINCT FACTORY_ID FROM [escmowner].[GEN_FACTORY] FI WHERE ACTIVE = 'Y' AND INTERNAL_FLAG = 'Y' AND OU IS NOT NULL"
     let res = await this.ctx.model2.query(sql);
     let returnData = [];
     res[0].forEach(item=>{
       returnData.push(item.FACTORY_ID);
     });
+    await ctx.helper.setStoreData(cacheKey,returnData,60*60*12);
     return returnData;
 
   }
