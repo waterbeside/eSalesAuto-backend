f0f9cada22f885f6343d02151cf3ccaf31641118
waterbeside <waterbeside@163.com>
2019-05-08 12:11:01 +0800

    feat: api for get wash types

--- app/service/GenWashType.js
+++ app/service/GenWashType.js
@@ -10,7 +10,24 @@ class GenWashTypeService extends BaseService {
     let res = await this.ctx.model2.query(sql);
     console.log(res[0][0].c);
     return res[0][0].c;
+  }
 
+  
+  async washTypeList(){
+    const { ctx , app} = this;
+    let cacheKey = "escm:gen_wash_type:wash_type_desc_list";
+    let cacheData = await ctx.helper.getStoreData(cacheKey);
+    if(cacheData){
+      return cacheData;
+    }
+    let sql = "SELECT DISTINCT WASH_TYPE_DESC FROM  escmowner.GEN_WASH_TYPE ORDER BY WASH_TYPE_DESC";
+    let res = await this.ctx.model2.query(sql);
+    let returnData = [];
+    res[0].forEach(item=>{
+      returnData.push(item.WASH_TYPE_DESC);
+    });
+    await ctx.helper.setStoreData(cacheKey,returnData,60*60*12);
+    return returnData;
   }
 }
 
