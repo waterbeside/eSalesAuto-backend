ec9df176828afe47af95d58b55f7c0842628fddf
waterbeside <waterbeside@163.com>
2019-05-16 10:42:10 +0800

    feat: GenCountry model and service

---
+++ app/model2/GenCountry.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = app => {
+  const { STRING, INTEGER, DATE } = app.Sequelize;
+
+  const GenCountry = app.model2.define('GEN_COUNTRY', {
+      COUNTRY_CD: {
+        type:STRING(10),
+        primaryKey: true,
+      },
+      NAME  : STRING(50),
+      MARKET_CD: STRING(10),
+      NAME_CHN: INTEGER(100),
+      ACTIVE: STRING(1),
+    },
+    {freezeTableName: true,
+      timestamps: false, 
+      schema: 'escmowner',
+      tableName:'GEN_COUNTRY'
+    }
+  );
+  // GenCountry.removeAttribute('id');
+
+  return GenCountry;
+};
