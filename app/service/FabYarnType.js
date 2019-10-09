51bccc36ecf84198e02af0dcc9d626eda22d7286
waterbeside <waterbeside@163.com>
2019-10-09 11:18:29 +0800

    feat: 读取 FAB_TANRN_TYPE 数据

---
+++ app/service/FabYarnType.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const BaseService = require('./Base');
+class FabYarnTypeService extends BaseService {
+
+
+  async findByDesc(YARN_TYPE_DESC, exp = 60 * 3) {
+    if (!YARN_TYPE_DESC) {
+      return false;
+    }
+    const cacheKey = 'm2:FAB_TARN_TYPE:DESC_' + YARN_TYPE_DESC;
+    if (typeof (exp) === 'number' && exp > -1) {
+      const cacheData = await this.ctx.helper.cache(cacheKey);
+      if (cacheData) {
+        return cacheData;
+      }
+    }
+    let sql = 'select * from ESCMOWNER.FAB_TARN_TYPE ';
+    sql += " where YARN_TYPE_DESC = '" + YARN_TYPE_DESC + "'";
+    const res = await this.query('oracle', sql, 1);
+    if (typeof (exp) === 'number' && exp > -1) {
+      await this.ctx.helper.cache(cacheKey, res, exp);
+    }
+    return res;
+  }
+}
+
+module.exports = FabYarnTypeService;
