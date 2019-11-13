12e0bc77530dd866118e048f359b27670b7a31dd
waterbeside <waterbeside@163.com>
2019-11-13 16:05:20 +0800

    feat: 添加‘查询Customer_Fab_Code是否唯一’的方法

--- app/model/MasterFabricationLN.js
+++ app/model/MasterFabricationLN.js
@@ -2,10 +2,10 @@
 /**
  * Master_Fabrication_LN (面料具体信息)
  */
-
+const moment = require('moment');
 const helper = require('../extend/helper');
 module.exports = app => {
-  const { STRING, INTEGER } = app.Sequelize;
+  const { STRING, INTEGER, DATE } = app.Sequelize;
 
   const MasterFabricationLN = app.model.define('Master_Fabrication_LN',
     {
@@ -31,6 +31,18 @@ module.exports = app => {
       Yarn_Type: STRING(300),
       Fab_Desc: STRING(1000),
       Fab_Remark: STRING(100),
+      Create_Time: {
+        type: DATE,
+        get() {
+          return moment(moment(this.getDataValue('Create_Time')).utc().format('YYYY-MM-DD HH:mm:ss'));
+        },
+      },
+      Update_Time: {
+        type: DATE,
+        get() {
+          return moment(moment(this.getDataValue('Update_Time')).utc().format('YYYY-MM-DD HH:mm:ss'));
+        },
+      },
     },
     { freezeTableName: true,
       timestamps: false,
@@ -59,6 +71,30 @@ module.exports = app => {
       await helper.cache({ app }).run(cacheKey, res, exp);
     }
     return res;
+  };
+
+  /**
+   * 检验Customer_Fab_Code唯一
+   * @param {String} Customer_Fab_Code 用户名
+   * @param {Integer} exclude_id 排除id
+   */
+  MasterFabricationLN.checkUnique = async function(Customer_Fab_Code, exclude_id) {
+    const Op = app.Sequelize.Op;
+    const where = {
+      Customer_Fab_Code,
+    };
+    if (exclude_id) {
+      where.id = {
+        [Op.ne]: exclude_id,
+      };
+    }
+    const cnt = await this.count({ where });
+    if (cnt > 0) {
+      return false;
+    }
+    return true;
+
+
   };
 
   return MasterFabricationLN;
